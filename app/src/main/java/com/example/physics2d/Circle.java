aa5e32c19166268aa72441a058a61e7819baba6f
RUSLoker <klokotkov@ya.ru>
2020-01-26 16:48:12 +0300

    Circle, Polygon additions

--- app/src/main/java/com/example/physics2d/Circle.java
+++ app/src/main/java/com/example/physics2d/Circle.java
@@ -3,7 +3,7 @@ package com.example.physics2d;
 public class Circle extends Figure2D {
     @Override
     public Vector2D getCollision(Figure2D f) {
-        return null;
+        return f.getCollision((Circle) this);
     }
 
     @Override
--- app/src/main/java/com/example/physics2d/Polygon.java
+++ app/src/main/java/com/example/physics2d/Polygon.java
@@ -3,7 +3,7 @@ package com.example.physics2d;
 public class Polygon extends Figure2D {
     @Override
     public Vector2D getCollision(Figure2D f) {
-        return null;
+        return f.getCollision((Polygon) this);
     }
 
     @Override
