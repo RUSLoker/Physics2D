894bb45c7e60bd9effc7d2fb7f82a6b04a806891
RUSLoker <klokotkov@ya.ru>
2020-01-26 17:08:08 +0300

    Collision class created

--- app/src/main/java/com/example/physics2d/Circle.java
+++ app/src/main/java/com/example/physics2d/Circle.java
@@ -1,6 +1,7 @@
 package com.example.physics2d;
 
 public class Circle extends Figure2D {
+
     @Override
     public Vector2D getCollision(Figure2D f) {
         return f.getCollision((Circle) this);
---
+++ app/src/main/java/com/example/physics2d/Collision.java
@@ -0,0 +1,15 @@
+package com.example.physics2d;
+
+public final class Collision {
+    private Collision(){}
+
+    public Vector2D get(Circle a, Circle b){
+        return null;
+    }
+    public Vector2D get(Polygon a, Polygon b){
+        return null;
+    }
+    public Vector2D get(Circle a, Polygon b){
+        return null;
+    }
+}
