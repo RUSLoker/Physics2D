c5f4178041599a5c36a85a91c54b5a4c58f7053c
RUSLoker <klokotkov@ya.ru>
2020-02-22 00:31:19 +0300

    Visualization changes

--- app/src/main/java/com/example/physics2d/MainActivity.java
+++ app/src/main/java/com/example/physics2d/MainActivity.java
@@ -1,13 +1,92 @@
 package com.example.physics2d;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    static double G = 6.67 * Math.pow(10, -11);
+    static Border border = new Border(0, 1000, 1000, 0);
+    static public PhysObj[] objs = new PhysObj[]{
+//            new PhysObj(
+//                    new Circle( new Vector2D(500, 600), 30),
+//                    1000000000000000000.0
+//                    , new Vector2D(120, 0)
+//            ),
+//            new PhysObj(
+//                    new Circle( new Vector2D(500, 500), 50),
+//                    1000000000000000000.0
+//                    , new Vector2D(-100, 0)
+//            ),
+//            new PhysObj(
+//                    new Circle( new Vector2D(500, 200), 50),
+//                    5000000000.0
+//                    , new Vector2D(600, 0)
+//            ),
+//            new PhysObj(
+//                    new Circle( new Vector2D(500, 800), 50),
+//                    5000000000.0
+//                    , new Vector2D(-700, 0)
+//            )
+    };
+    Thread myThread = new Thread(this::vrun);
+    static boolean work = false;
+    int pointer = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(new Visualizer(this));
+        setContentView(R.layout.activity_main);
+        myThread.start();
+    }
+
+    protected void onPause(){
+        work = false;
+        super.onPause();
+    }
+
+    public void vrun() {
+        double time = 0.000005;
+        while (true) {
+            if (work) {
+                for (int i = 0; i < objs.length; i++) {
+                    PhysObj a = objs[i];
+                    for (int j = i + 1; j < objs.length; j++) {
+                        PhysObj b = objs[j];
+                        Vector2D dist = a.getCenter().sub(b.getCenter());
+                        double forceAbs = G * (a.getMass() * b.getMass()) / (dist.length * dist.length);
+                        forceAbs = forceAbs == 1.0 / 0.0 ? 0 : forceAbs;
+                        Vector2D force = dist.setLength(forceAbs);
+                        a.setForce(b.hashCode(), force.reverse());
+                        b.setForce(a.hashCode(), force);
+                    }
+                }
+                for (int i = 0; i < objs.length; i++) {
+                    PhysObj a = objs[i];
+                    for (int j = i + 1; j < objs.length; j++) {
+                        PhysObj b = objs[j];
+                        a.checkCollisions(b, time);
+                    }
+                }
+                for (PhysObj i : objs) {
+                    i.calcAccel();
+                    i.move(time);
+                    i.checkBorder(border);
+                }
+            }
+            else {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public void toggle(View view){
+        work = !work;
     }
 }
--- app/src/main/java/com/example/physics2d/Visualizer.java
+++ app/src/main/java/com/example/physics2d/Visualizer.java
@@ -2,17 +2,27 @@ package com.example.physics2d;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.AttributeSet;
 import android.view.View;
 
 public class Visualizer extends View {
-    public Visualizer(Context context) {
-        super(context);
 
+    Paint paint = new Paint();
+
+    public Visualizer(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        paint.setStrokeWidth(10);
+        paint.setStyle(Paint.Style.STROKE);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
+        for(PhysObj i : MainActivity.objs){
+            i.draw(canvas, paint);
+        }
+        invalidate();
     }
+
 }
