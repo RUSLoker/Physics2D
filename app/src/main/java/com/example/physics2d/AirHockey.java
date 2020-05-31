982d9a79eba86d035065e15dad4173f936cf3b6e
RUSLoker <klokotkov@ya.ru>
2020-05-31 21:35:40 +0300

    Immersive mode using

--- app/src/main/java/com/example/physics2d/AirHockey.java
+++ app/src/main/java/com/example/physics2d/AirHockey.java
@@ -54,6 +54,7 @@ public class AirHockey extends AppCompatActivity {
         manips[1] = new Manipulator();
         setDefault();
         myThread.start();
+        hideSystemUI();
     }
 
     void setDefault(){
@@ -278,5 +279,23 @@ public class AirHockey extends AppCompatActivity {
         }
         return true;
     }
-    
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
 }
--- app/src/main/java/com/example/physics2d/EndGameScreen.java
+++ app/src/main/java/com/example/physics2d/EndGameScreen.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -35,11 +36,30 @@ public class EndGameScreen extends AppCompatActivity {
         intent = new Intent(this, AirHockey.class);
         Button restart = findViewById(R.id.restart);
         restart.setOnClickListener((x) -> listener());
-
+        hideSystemUI();
     }
 
     void listener(){
         startActivity(intent);
         finish();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            hideSystemUI();
+        }
+    }
+
+    private void hideSystemUI() {
+        View decorView = getWindow().getDecorView();
+        decorView.setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
     }
 }
